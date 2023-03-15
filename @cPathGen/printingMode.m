eed06f776d7fda8724d8de546b8d3972f918956b
Xiaoke DENG <654024751@qq.com>
2023-03-16 00:06:13 +0800

    add G43H16 in cPathGen.printingMode()

--- @cPathGen/printingMode.m
+++ @cPathGen/printingMode.m
@@ -2,7 +2,8 @@ function ret = printingMode(obj)
 % printing mode along with G55 CS + update AIO
     fprintf(obj.fid_, "M94 ;;选择激光模式\r\n");
     fprintf(obj.fid_, "G55 ;; 激光打印选择G55坐标系\r\n");
-    fprintf(obj.fid_, "G49  ;;关闭T0的长度补偿\r\n");
+    fprintf(obj.fid_, "G49  ;;关闭以前的长度补偿\r\n");
+    fprintf(obj.fid_, "G43H16  ;;打开熔覆头的长度补偿\r\n");
     fprintf(obj.fid_, "M142 ;;开启模拟量插补\r\n");                 
     obj.curMode_ = 1;            
     ret = 1;
