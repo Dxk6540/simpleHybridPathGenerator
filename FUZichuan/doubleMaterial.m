f2dd467d48df238554d29b23fa0d34147ede3522
jianchenghao <56340726+jianchenghao@users.noreply.github.com>
2023-04-25 13:16:23 +0800

    0425

--- FUZichuan/doubleMaterial.m
+++ FUZichuan/doubleMaterial.m
@@ -1,7 +1,7 @@
 %%%%%%%%%%%%%%%%%%%
 %
 % date: 2023-4-24
-% author�? CHEN Yuanzhi
+% author: CHEN Yuanzhi
 %
 %%%%%%%%%%%%%%%%%%
 
@@ -9,7 +9,7 @@
 hFilename = strcat('./doubleMaterial',date,'.txt');
 
 % printing process param
-pwr = 300; % 1.2KW / 4kw *1000;
+pwr = 250; % 1.2KW / 4kw *1000;
 lenPos = 900;
 flowL = 250; % 6 L/min / 20L/min * 1000;
 speedL = 100;% 2 r/min / 10r/min * 1000;
@@ -21,21 +21,22 @@ step = 1;
 % machining process param
 mFeedrate = 1400; % mm/min
 spindleSpeed = 10000;
-toolNum = 1;
-toolRadiu = 4;
+toolNum = 3;
+toolRadiu = 5;
+machiningLyrThickness = 0.2;
 
 %  geometry param
-startCtr = [0,0];
-pLyrNum = 48;
-lyrHeight = 0.5;
+startCtr = [-60,0];
+pLyrNum = 36;
+lyrHeight = 0.35;
 cubeShape = [20,20];
 tol = 0.01;
 safetyHeight = 230;
 zOffset = 0;
-angle = 0:15:359;
+angle = 0:30:359;
 rotation = true;
 side = 1; % machining inside is -1 and outside is 1
-wallOffset = 1.1;
+wallOffset = -0.5;
 aus = true;
 allPath=[];
 
@@ -50,15 +51,15 @@ pg.genNewScript();
 pg.changeMode(1); % change to printing mode
 for i=1:2*pLyrNum
     aus=~aus;
-    [pPathSeq,pwrSeq,feedOffset] = handle.genPrintingPath(cubeShape, startCtr, 1, lyrHeight, round(max(100,(1-0.2/2/pLyrNum*i)*(pwr-(~aus)*100))), zOffset+floor((i-1)/2)*lyrHeight, angle(rem(floor((i-1)/2),length(angle))+1), false, step, aus);
+    [pPathSeq,pwrSeq,feedOffset] = handle.genPrintingPath(cubeShape, startCtr, 1, lyrHeight, round(max(100,(1-0.2/2/pLyrNum*i)*(pwr-(~aus)*50))), zOffset+(i-1)*lyrHeight, angle(rem(floor((i-1)/2),length(angle))+1), false, step, aus);
     lenPosSeq = ones(length(pPathSeq),1) * lenPos;
     if aus
-       flipud(pPathSeq); 
-       flipud(pwrSeq);
+       pPathSeq = flipud(pPathSeq); 
+       pwrSeq = flipud(pwrSeq);
     end
     %%%%%%%%%%%%% following for path Gen %%%%%%%%%%%%%%%%%%%%%
     %%% start printing mode
-    pg.setLaser(pwr, lenPos, flowL, aus*speedL, flowR, (~aus)*speedR); % set a init process param (in case of overshoot)
+    pg.setLaser(0, lenPos, flowL, aus*speedL, flowR, (~aus)*speedR); % set a init process param (in case of overshoot)
     pg.saftyToPt([nan, nan, safetyHeight], [startCtr(1), startCtr(2), zOffset+floor((i-1)/2)*lyrHeight], 3000); % safety move the start pt
     pg.pauseProgram();% pause and wait for start (the button)
     pg.enableLaser(3, 10);
@@ -66,7 +67,7 @@ for i=1:2*pLyrNum
     pg.addPathPtsWithPwr(pPathSeq, pwrSeq, lenPosSeq, pFeedrate*feedOffset-(~aus)*200);
     %%% exist printing mode
     pg.disableLaser(3);
-    pg.draw_ = true;
+    pg.draw_ = false;
     pg.drawPath(pPathSeq, pPathSeq);
     allPath = [allPath;pPathSeq];
 end
@@ -75,7 +76,7 @@ pg.drawPath(allPath, allPath);
 
 %%
 %%%%%%%%%%%%%% machining path
-mPathSeq = handle.genMachiningPath(cubeShape, startCtr, pLyrNum*lyrHeight, lyrHeight, toolRadiu, wallOffset, zOffset);
+mPathSeq = handle.genMachiningPath(cubeShape, startCtr, 2*pLyrNum*lyrHeight, 2*lyrHeight, toolRadiu, wallOffset, zOffset);
 %%% start machining mode
 pg.changeMode(2); % change to machining mode
 pg.changeTool(toolNum);
@@ -87,6 +88,19 @@ pg.addPathPts(mPathSeq, mFeedrate);
 %%% exist machining mode
 pg.disableSpindle();
 pg.returnToSafety(safetyHeight, 3000);
+
+mPathSeq = planarMachining([startCtr(1)+cubeShape(1)/2, startCtr(2)+cubeShape(2)/2], [zOffset+2*pLyrNum*lyrHeight+3, zOffset+2*pLyrNum*lyrHeight], cubeShape, machiningLyrThickness, toolRadiu);
+pg.changeMode(2); % change to machining mode
+pg.changeTool(toolNum);
+pg.saftyToPt([nan, nan, safetyHeight], [startCtr(1) - toolRadiu - wallOffset - 5, startCtr(2) - toolRadiu - wallOffset - 5, pLyrNum * lyrHeight], 3000); % safety move the start pt
+pg.pauseProgram();% pause and wait for start (the button)
+pg.enableSpindle(spindleSpeed, toolNum); % set a init process param (in case of overshoot)
+%%% add path pts
+pg.addPathPts(mPathSeq, mFeedrate);
+%%% exist machining mode
+pg.disableSpindle();
+pg.returnToSafety(safetyHeight, 3000);
+
 %%% end the script
 pg.closeScript();
 % pg.draw_ = true;
