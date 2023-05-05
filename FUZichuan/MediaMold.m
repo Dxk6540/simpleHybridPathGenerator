624948799cf9d9c3a21c049d72022570173fa111
CHEN-Yuanzhi <84832730+CHEN-Yuanzhi@users.noreply.github.com>
2023-05-05 17:52:33 +0800

    0505

--- FUZichuan/MediaMold.m
+++ FUZichuan/MediaMold.m
@@ -1,10 +1,3 @@
-%%%%%%%%%%%%%%%%%%%
-%
-% with RTCP function.
-% date: 2023-3-3
-%
-%%%%%%%%%%%%%%%%%%
-
 % file param:
 hFilename = strcat('./MediaMold',date,'.txt');
 
@@ -13,75 +6,123 @@ pwr = 200; % 1.2KW / 4kw *1000;
 lenPos = 900;
 flowL = 250; % 6 L/min / 20L/min * 1000;
 speedL = 100;% 2 r/min / 10r/min * 1000;
-flowR = 250;% 6 L/min / 20L/min * 1000;
+flowR = 400;% 6 L/min / 20L/min * 1000;
 speedR = 100;% 2 r/min / 10r/min * 1000;
-pFeedrate = 400; % mm/min
-channel = 1;
+pFeedrate = 500; % mm/min
 step = 1;
+channel = 1;
 
 % machining process param
-mFeedrate = 400; % mm/min
+mFeedrate = 200; % mm/min
 spindleSpeed = 10000;
 toolNum = 1;
 toolRadiu = 1;
+machiningLyrThickness = 0.1;
+planarMachiningDepth = 1.5;
 
 %  geometry param
 startCtr = [0,0];
 pLyrNum = 10;
 lyrHeight = 0.5;
-radius = 1.5;
+radius = 1.8;
 tol = 0.01;
 safetyHeight = 230;
 zOffset = 0;
-side = -1; % machining inside is -1 and outside is 1
-wallOffset = 0.475;
-rollAgl = 0;
+side = 1; % machining inside is -1 and outside is 1
+outterWallOffsetRng = [0.8, 0];
+innerWallOffsetRng = [0.8, 0.775];
+sglWpHeight = pLyrNum * lyrHeight;
+alterNum = 0;
+zOffsetRng = [zOffset, zOffset + sglWpHeight*alterNum]; 
+allPath=[];
 
 % shape
-handle=hollowCylinder;
+handle = hollowCylinder;
 
+%%
+%%%%%%%%%%%%%% printing path
 %%%% the regular code for generate a script
 pg = cPathGen(hFilename); % create the path generator object
 pg.genNewScript();
 
-%%
-%%%%%%%%%%%%%% printing path
-[pPathSeq,pwrSeq] = handle.genPrintingPath(radius, startCtr, tol, pLyrNum, lyrHeight, pwr, zOffset, channel, step);
-lenPosSeq = ones(length(pPathSeq),1) * lenPos;
-%%%%%%%%%%%%% following for path Gen %%%%%%%%%%%%%%%%%%%%%
-%%% start printing mode
-pg.changeMode(1); % change to printing mode
-pg.setLaser(pwr, lenPos, flowL, speedL, flowR, speedR); % set a init process param (in case of overshoot)
-pg.saftyToPt([nan, nan, safetyHeight], [startCtr(1) + radius, startCtr(2), 0], 3000); % safety move the start pt
-pg.pauseProgram();% pause and wait for start (the button)
-pg.enableLaser(1, 10);
-%%% add path pts
-pg.addPathPtsWithPwr(pPathSeq, pwrSeq, lenPosSeq, pFeedrate);
-%%% exist printing mode
-pg.disableLaser(1);
-% %%% end the script
+for zOffset = zOffsetRng(1): sglWpHeight: zOffsetRng(2)
+    %%%%%%% gen printing process %%%%%%
+    pg.addCmd(";;;;;start a printing process;;;;;;;;;;");
+    pg.changeMode(1);
+    [pPathSeq, pwrSeq, feedOffset] = handle.genPrintingPath(radius, startCtr, tol, pLyrNum, lyrHeight, ...
+                                        pwr, zOffset, channel, step);
+    lenPosSeq = ones(length(pPathSeq),1) * lenPos;
+    pg.setLaser(0, lenPos, flowL, speedL, flowR, speedR); % set a init process param (in case of overshoot)
+    pg.saftyToPt([nan, nan, safetyHeight], pPathSeq(1,:), 3000); % safety move the start pt
+    pg.pauseProgram();% pause and wait for start (the button)
+    pg.enableLaser(1, 10);
+    %%% add path pts
+    pg.addPathPtsWithPwr(pPathSeq, pwrSeq, lenPosSeq, pFeedrate);
+    %%% exist printing mode
+    pg.disableLaser(1);
+    pg.draw_ = false;
+    pg.drawPath(pPathSeq, pPathSeq);
+    allPath = [allPath;pPathSeq];
+    
+    %%%%%%%%%%%%%%%%%%%%% plannar circle machining %%%%%%%%%%%%%%%%%%%%
+    pg.addCmd(";;;;;start a planar machining process");
+    depthRng = [sglWpHeight+zOffset+planarMachiningDepth, sglWpHeight+zOffset];
+    toolNum = 3;
+    toolRadiu = 5;
+    [mpPath, mpFeedSeq] = planarCircleMachining(startCtr, depthRng, [0,3], -machiningLyrThickness, toolRadiu, mFeedrate, mFeedrate);   
+    pg.changeMode(2); % change to machining mode
+    pg.changeTool(toolNum);
+    pg.saftyToPt([nan, nan, safetyHeight], [startCtr(1), startCtr(2), zOffset + pLyrNum * lyrHeight + 5], 3000); % safety move the start pt
+    pg.pauseProgram();% pause and wait for start (the button)
+    pg.enableSpindle(spindleSpeed, toolNum); % set a init process param (in case of overshoot)
+    %%% add path pts
+    pg.addPathPts(mpPath, mpFeedSeq);
+    %%% exist machining mode
+    pg.disableSpindle();
+    pg.returnToSafety(safetyHeight, 3000);
+    
+    %%%%%%%%%%%%%%%%%%%%%%%% gen outter machining process %%%%%%%%%%%%%%%%%%%%%%%
+    pg.addCmd(";;;;;start an outter machining process");    
+    side = 1;
+    totalMachiningPath = [];
+    toolNum = 3;
+    toolRadiu = 5;
+    for wallOffset = outterWallOffsetRng(1) : -machiningLyrThickness : outterWallOffsetRng(2)
+        mPathSeq = handle.genMachiningPath(radius, startCtr, tol, pLyrNum * lyrHeight, lyrHeight, toolRadiu, wallOffset, zOffset, side);
+        totalMachiningPath = [totalMachiningPath; mPathSeq]; 
+        pg.changeTool(toolNum);
+        pg.saftyToPt([nan, nan, safetyHeight], [startCtr(1)-toolRadiu-radius-wallOffset-5, startCtr(2)-toolRadiu-radius-wallOffset-5, zOffset + pLyrNum * lyrHeight + 5], 3000); % safety move the start pt
+        pg.pauseProgram();% pause and wait for start (the button)
+        pg.enableSpindle(spindleSpeed, toolNum); % set a init process param (in case of overshoot)
+        %%% add path pts
+        pg.addPathPts(totalMachiningPath, mFeedrate);
+        %%% exist machining mode
+        pg.disableSpindle();
+        pg.returnToSafety(safetyHeight, 3000);
+    end
 
+    
+    %%%%%%%%%%%%%%%%%%%%%%%% gen inner machining process %%%%%%%%%%%%%%%%%%%%%%%
+    pg.addCmd(";;;;;start an inner machining process");
+    side = -1;
+    totalMachiningPath = [];
+    toolNum = 1;
+    toolRadiu = 1;
+    for wallOffset = innerWallOffsetRng
+        mPathSeq = handle.genMachiningPath(radius, startCtr, tol, pLyrNum * lyrHeight, lyrHeight, toolRadiu, wallOffset, zOffset, side);
+        totalMachiningPath = [totalMachiningPath; mPathSeq]; 
+        pg.changeTool(toolNum);
+        pg.saftyToPt([nan, nan, safetyHeight], [startCtr(1), startCtr(2), zOffset + pLyrNum * lyrHeight + 5], 3000); % safety move the start pt
+        pg.pauseProgram();% pause and wait for start (the button)
+        pg.enableSpindle(spindleSpeed, toolNum); % set a init process param (in case of overshoot)
+        %%% add path pts
+        pg.addPathPts(totalMachiningPath, mFeedrate);
+        %%% exist machining mode
+        pg.disableSpindle();
+        pg.returnToSafety(safetyHeight, 3000);        
+    end
+end
 
-%%
-%%%%%%%%%%%%%% machining path
-mPathSeq = handle.genMachiningPath(radius, startCtr, tol, pLyrNum * lyrHeight, lyrHeight, toolRadiu, wallOffset, zOffset, rollAgl, side, 5);
-
-%%% start machining mode
-pg.changeMode(2); % change to machining mode
-pg.changeTool(toolNum);
-pg.saftyToPt([nan, nan, safetyHeight], [startCtr(1) + side*(radius + toolRadiu + wallOffset + 5), startCtr(2), pLyrNum * lyrHeight], 3000); % safety move the start pt
-pg.pauseProgram();% pause and wait for start (the button)
-pg.enableSpindle(spindleSpeed, toolNum); % set a init process param (in case of overshoot)
-%%% add path pts
-pg.addPathPts(mPathSeq, mFeedrate);
-%%% exist machining mode
-pg.disableSpindle();
-pg.returnToSafety(safetyHeight, 3000);
 %%% end the script
 pg.closeScript();
-
-%%% draw the path
-pg.draw_ = true;
-pg.drawPath(pPathSeq, mPathSeq);
-
 
--- lib/shape/hollowCylinder.m
+++ lib/shape/hollowCylinder.m
@@ -2,21 +2,78 @@ classdef hollowCylinder
     %HOLLOWCYLINDER 此处显示有关此类的摘要
     %   此处显示详细说明
     
-    properties
-        Property1
+   	properties
+        shape_="Cylinder";
     end
-    
-    methods
-        function obj = hollowCylinder(inputArg1,inputArg2)
-            %HOLLOWCYLINDER 构造此类的实例
-            %   此处显示详细说明
-            obj.Property1 = inputArg1 + inputArg2;
+    methods(Static)
+        function [path,pwrSeq,feedrateOffset] = genPrintingPath(cylinderR, startCenter, tol, lyrNum, lyrThickness, pwr, zOffset, channel, step)
+            % planar circle path
+            lyrPtNum = floor(2 * cylinderR * pi / tol)+1;
+            aglStep = 2 * pi / lyrPtNum; 
+            path = [];
+            pwrSeq = [];
+            feedrateOffset = [];
+            for lyrIdx = 0 : lyrNum - 1    
+                tPathSeq = [];
+                tPwrSeq = [];
+                if channel > 1
+                    for chnIdx = 0 : channel - 1
+                        for j = 0 : lyrPtNum - 1
+                            x = cos(aglStep * j) * (cylinderR - chnIdx * step) + startCenter(1);
+                            y = sin(aglStep * j) * (cylinderR - chnIdx * step) + startCenter(2);
+                            z = lyrIdx * lyrThickness + zOffset;
+                            speedOffset = (1.025-abs(aglStep * j-0.75*pi)/pi*0.05);
+                            tPathSeq = [tPathSeq; x,y,z];
+                            tPwrSeq = [tPwrSeq; pwr];
+                            feedrateOffset = [feedrateOffset;speedOffset];
+                        end
+                        tPwrSeq(1)= 0;
+                        tPwrSeq(end) = 0;                
+                    end
+                else
+                   for j = 0 : lyrPtNum - 1
+                        x = cos(aglStep * j) * cylinderR + startCenter(1);
+                        y = sin(aglStep * j) * cylinderR + startCenter(2);
+                        z = lyrIdx * lyrThickness + zOffset + j * lyrThickness / lyrPtNum;
+                        tPathSeq = [tPathSeq; x,y,z];
+                        tPwrSeq = [tPwrSeq; pwr];
+                    end
+                end
+                % stop the power when lift the tool 
+                path = [path;tPathSeq];
+                pwrSeq = [pwrSeq;tPwrSeq];
+            end
+            pwrSeq(1) = pwr;
         end
-        
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 此处显示有关此方法的摘要
-            %   此处显示详细说明
-            outputArg = obj.Property1 + inputArg;
+
+        function path = genMachiningPath(cylinderR, startCenter, tol, wpHeight, lyrThickness, toolRadiu, wallOffset, zOffset, side)
+            % planar circle path
+            lyrPtNum = floor(2 * (cylinderR + side*(toolRadiu + wallOffset)) * pi / tol)+1;
+            % wpHeight = lyrNum * lyrHeight;
+            if floor(wpHeight/lyrThickness) == wpHeight/lyrThickness
+                lyrNum = floor(wpHeight/lyrThickness);            
+            else
+                lyrNum = floor(wpHeight/lyrThickness) + 1;    
+            end
+            lyrHeight = wpHeight/lyrNum;
+
+            if zOffset > 0
+                lyrNum = lyrNum + 6;
+            end
+
+            aglStep = 2 * pi / lyrPtNum;
+            mPathSeq = [];
+            for lyrIdx = 1:lyrNum+1
+            %     centerXOffset = ((lyrIdx - 1) * lyrHeight) * tan(inclinationAgl/180 * pi); 
+                for j = 1 : lyrPtNum
+            %         x = cos(aglStep * j) * radius + startCenter(1) + centerXOffset;
+                    x = cos(aglStep * j) * (cylinderR + side*(toolRadiu + wallOffset)) + startCenter(1);
+                    y = sin(aglStep * j) * (cylinderR + side*(toolRadiu + wallOffset)) + startCenter(2);
+                    z = max(0.06, wpHeight - (lyrIdx - 1) * lyrHeight + zOffset);
+                    mPathSeq = [mPathSeq; x,y,z];
+                end
+            end
+            path = mPathSeq;
         end
     end
 end
