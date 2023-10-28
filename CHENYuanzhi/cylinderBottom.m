cdc15e973dd7e7ccd5251bc3172d3ac8d77acfcd
CHEN-Yuanzhi <84832730+CHEN-Yuanzhi@users.noreply.github.com>
2023-10-28 09:10:44 +0800

    1028

--- CHENYuanzhi/cylinderBottom.m
+++ CHENYuanzhi/cylinderBottom.m
@@ -2,13 +2,13 @@
 hFilename = strcat('./CylinderFixing',date,'.txt');
 
 % printing process param
-pwr = 250; % 1.2KW / 4kw *1000;
+pwr = 200; % 1.2KW / 4kw *1000;
 lenPos = 900;
 flowL = 250; % 6 L/min / 20L/min * 1000;
 speedL = 100;% 2 r/min / 10r/min * 1000;
 flowR = 250;% 6 L/min / 20L/min * 1000;
 speedR = 100;% 2 r/min / 10r/min * 1000;
-pFeedrate = 600; % mm/min
+pFeedrate = 650; % mm/min
 step = 0.8;
 channel = 5;
 
@@ -22,9 +22,9 @@ planarMachiningDepth = 3;
 
 %  geometry param
 startCtr = [0,0];
-pLyrNum = 20;
-lyrHeight = 0.6;
-radius = 35;
+pLyrNum = 80;
+lyrHeight = 0.5;
+radius = 29.7;
 tol = 0.1;
 safetyHeight = 230;
 zOffset = 0;
@@ -32,7 +32,7 @@ side = 1; % machining inside is -1 and outside is 1
 outterWallOffsetRng = [0.8, 0];
 innerWallOffsetRng = [radius-1, radius-1.025];
 sglWpHeight = pLyrNum * lyrHeight;
-alterNum = 0;
+alterNum = 5;
 zOffsetRng = [zOffset, zOffset + sglWpHeight*alterNum]; 
 allPath=[];
 
@@ -50,9 +50,10 @@ for zOffset = zOffsetRng(1): sglWpHeight: zOffsetRng(2)
     pg.addCmd(";;;;;start a printing process;;;;;;;;;;");
     pg.changeMode(1);
     [pPathSeq, pwrSeq, feedOffset] = handle.genPrintingPath(radius, startCtr, tol, pLyrNum, lyrHeight, ...
-                                        pwr, zOffset, channel, step);
+                                        pwr, zOffset, channel, step,2,5,5);
     lenPosSeq = ones(length(pPathSeq),1) * lenPos;
     pg.setLaser(0, lenPos, flowL, speedL, flowR, speedR); % set a init process param (in case of overshoot)
+    pg.startRTCP(safetyHeight, 16);
     pg.saftyToPt([nan, nan, safetyHeight], pPathSeq(1,:), 3000); % safety move the start pt
     pg.pauseProgram();% pause and wait for start (the button)
     pg.enableLaser(1, 10);
@@ -60,11 +61,10 @@ for zOffset = zOffsetRng(1): sglWpHeight: zOffsetRng(2)
     pg.addPathPtsWithPwr(pPathSeq, pwrSeq, lenPosSeq, pFeedrate);
     %%% exist printing mode
     pg.disableLaser(1);
+    pg.stopRTCP(safetyHeight, 16);  
     pg.draw_ = false;
     pg.drawPath(pPathSeq, pPathSeq);
     allPath = [allPath;pPathSeq];
 end
-pg.draw_ = true;
-pg.drawPath(allPath, allPath);
 %%% end the script
 pg.closeScript();
--- lib/shape/hollowCylinder.m
+++ lib/shape/hollowCylinder.m
@@ -6,8 +6,10 @@ classdef hollowCylinder
         shape_="Cylinder";
     end
     methods(Static)
-        function [path,pwrSeq,feedrateOffset] = genPrintingPath(cylinderR, startCenter, tol, lyrNum, lyrThickness, pwr, zOffset, channel, step)
+        function [path,pwrSeq,feedrateOffset] = genPrintingPath(cylinderR, startCenter, tol, lyrNum, lyrThickness, pwr, zOffset, channel, step, inLayerShift, interShift, leanAngle)
             % planar circle path
+            inLayerShiftPtNum = round(inLayerShift/tol);
+            interShiftPtNum = round(interShift/tol);
             lyrPtNum = floor(2 * cylinderR * pi / tol)+1;
             aglStep = 2 * pi / lyrPtNum; 
             path = [];
@@ -17,23 +19,58 @@ classdef hollowCylinder
                 tPathSeq = [];
                 tPwrSeq = [];
                 for chnIdx = 0 : channel - 1
-                    for j = 0 : lyrPtNum - 1
-                        x = cos(aglStep * j + 0.5*lyrIdx) * (cylinderR - chnIdx * step) + startCenter(1);
-                        y = sin(aglStep * j + 0.5*lyrIdx) * (cylinderR - chnIdx * step) + startCenter(2);
+                    firstPt = lyrIdx*interShiftPtNum+chnIdx*inLayerShiftPtNum;
+                    lastPt = lyrPtNum  + firstPt;
+                    x = cos(aglStep * firstPt) * (cylinderR - chnIdx * step) + startCenter(1) + sin(aglStep * firstPt)*interShift;
+                    y = sin(aglStep * firstPt) * (cylinderR - chnIdx * step) + startCenter(2) - cos(aglStep * firstPt)*interShift;
+                    z = lyrIdx * lyrThickness + zOffset;
+                    b = 0;
+                    c = (aglStep * firstPt)*180/pi;
+                    tPathSeq = [tPathSeq; x,y,z,b,c];
+                    tPwrSeq = [tPwrSeq; 0];
+                    feedrateOffset = [feedrateOffset;1];
+                    for j = firstPt : lastPt
+                        x = cos(aglStep * j) * (cylinderR - chnIdx * step) + startCenter(1);
+                        y = sin(aglStep * j) * (cylinderR - chnIdx * step) + startCenter(2);
                         z = lyrIdx * lyrThickness + zOffset;
-                        speedOffset = (1.025-abs(aglStep * j-0.75*pi)/pi*0.05);
-                        tPathSeq = [tPathSeq; x,y,z];
+                        if chnIdx == 0
+                            b = leanAngle;
+                            c = (aglStep * j)*180/pi;
+                        elseif chnIdx == channel - 1
+                            b = -leanAngle;
+                            c = (aglStep * j)*180/pi;
+                        else
+                            b=0;
+                            c=0;
+                        end
+                        speedOffset = 1;
+                        if j == firstPt
+                            tPathSeq = [tPathSeq; x,y,z,b,c];
+                            tPwrSeq = [tPwrSeq; 0];
+                            feedrateOffset = [feedrateOffset;speedOffset];
+                        end
+                        tPathSeq = [tPathSeq; x,y,z,b,c];
                         tPwrSeq = [tPwrSeq; pwr];
                         feedrateOffset = [feedrateOffset;speedOffset];
+                        if j == lastPt
+                            tPathSeq = [tPathSeq; x,y,z,b,c];
+                            tPwrSeq = [tPwrSeq; 0];
+                            feedrateOffset = [feedrateOffset;speedOffset];
+                        end                        
                     end
-                    tPwrSeq(1)= 0;
-                    tPwrSeq(end) = 0;                
+                    x = cos(aglStep * lastPt) * (cylinderR - chnIdx * step) + startCenter(1) - sin(aglStep * firstPt)*interShift;
+                    y = sin(aglStep * lastPt) * (cylinderR - chnIdx * step) + startCenter(2) + cos(aglStep * firstPt)*interShift;
+                    z = lyrIdx * lyrThickness + zOffset;
+                    b = 0;
+                    c = (aglStep * firstPt)*180/pi;
+                    tPathSeq = [tPathSeq; x,y,z,b,c];
+                    tPwrSeq = [tPwrSeq; 0];
+                    feedrateOffset = [feedrateOffset;1];             
                 end
                 % stop the power when lift the tool 
                 path = [path;tPathSeq];
                 pwrSeq = [pwrSeq;tPwrSeq];
             end
-            pwrSeq(1) = pwr;
         end
 
         function path = genMachiningPath(cylinderR, startCenter, tol, wpHeight, lyrThickness, toolRadiu, wallOffset, zOffset, side)
