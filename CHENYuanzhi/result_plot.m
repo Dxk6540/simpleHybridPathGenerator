1b1c1bc6a8c265664f0eab751e9088e800fb867b
CHEN-Yuanzhi <84832730+CHEN-Yuanzhi@users.noreply.github.com>
2023-11-30 18:24:30 +0800

    1130

--- CHENYuanzhi/result_plot.m
+++ CHENYuanzhi/result_plot.m
@@ -6,9 +6,11 @@ hold on
 x2=300:100:1200;
 y2=0.1:0.1:0.9;
 [X2,Y2]=meshgrid(x2,y2);
-surf(X2,Y2,cat(2,heights2,heights3));
+surf(X2,Y2,heights23);
 hold on
 x4=1300:100:1700;
 y4=0.1:0.1:0.3;
 [X4,Y4]=meshgrid(x4,y4);
-surf(X4,Y4,heights3(1:3,:));
+surf(X4,Y4,heights4(1:3,:));
+error23=heights23-6*y2';
+surf(X2,Y2,error23);
