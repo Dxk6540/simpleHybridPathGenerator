764100e3c55b63e0de0a46150bf1c07f8f7ea3e9
jianchenghao <56340726+jianchenghao@users.noreply.github.com>
2024-03-23 09:26:07 +0800

    Update inclinePrinted.m

--- CHENYuanzhi/inclinePrinted.m
+++ CHENYuanzhi/inclinePrinted.m
@@ -9,23 +9,23 @@
 hFilename = strcat('./inclinePrintedWall',date,'.txt');
 
 % printing process param
-pwr = 250; % 1.2KW / 4kw *1000;
+pwr = 180; % 1.2KW / 4kw *1000;
 lenPos = 900;
 flowL = 250; % 6 L/min / 20L/min * 1000;
 speedL = 100;% 2 r/min / 10r/min * 1000;
 flowR = 400;% 6 L/min / 20L/min * 1000;
 speedR = 100;% 2 r/min / 10r/min * 1000;
-pFeedrate = 500; % mm/min
+pFeedrate = 160; % mm/min
 step = 0.8;
-safetyHeight = 200;
+safetyHeight = 25;
 
 %  geometry param
-startCtr = [-30,20];
-pLyrNum = 40;
+startCtr = [0,0];
+pLyrNum = 30;
 lyrHeight = 0.5;
-wallLength = 10;
-clearance=10;
-lead = 2.5;
+wallLength = 20;
+clearance=-10;
+lead = 1;
 inclineAngle = 0;
 channel = 1;
 
@@ -38,17 +38,17 @@ handle = inclinePrintedWall;
 pg = cPathGen(hFilename); % create the path generator object
 pg.genNewScript();
 totalPath=[];
-for inclineAngle = 0: -5: -45
+for inclineAngle = -40: -5: -45
     pg.changeMode(1); % change to printing mode
     [pPathSeq,pwrSeq] = handle.genPrintingPath(wallLength, startCtr, lead, pLyrNum, lyrHeight, pwr, inclineAngle, channel, step);
     lenPosSeq = ones(length(pPathSeq),1) * lenPos;
     %%%%%%%%%%%%% following for path Gen %%%%%%%%%%%%%%%%%%%%%
     %%% start printing mode
     pg.setLaser(0, lenPos, flowL, speedL, flowR, speedR); % set a init process param (in case of overshoot)
-    pg.addPathPts([0,0,safetyHeight,inclineAngle,0], pFeedrate);
+    pg.addPathPts([0,0,safetyHeight,inclineAngle,0], 3000);
     pg.startRTCP(safetyHeight, 16);
     pg.saftyToPt([nan, nan, safetyHeight], pPathSeq(1,:), 3000); % safety move the start pt
-    pg.enableLaser(1, 10);
+    pg.enableLaser(1, 5);
     %%% add path pts
     pg.addPathPtsWithPwr(pPathSeq, pwrSeq, lenPosSeq, pFeedrate);
     %%% exist printing mode
