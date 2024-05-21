303767cd24a3a6dc85384300d91fb1ec273bfeb0
Xiaoke DENG <654024751@qq.com>
2024-05-21 15:28:08 +0800

    kk revised2

--- kk/roll/convert2DPoint.m
+++ kk/roll/convert2DPoint.m
@@ -13,8 +13,8 @@ function [point_3,ori_3,height,on_off,traverse]=convert2DPoint(point,on_off,trav
         traverse=[traverse;ones(count-1,1)];
     end
     
-    angle=(point(:,2)-center(2))*180/pi/radius;
-    height=offset+point(:,1)-center(1);
+    angle=(point(:,2)+center(2))*180/pi/radius;
+    height=offset+point(:,1)+center(1);
     point_3=[];
     for i=1-remelt:lyrNum
         point_3=[point_3;(radius+max(0,i-1)*lyrHeight)*cos(angle/180*pi),-(radius+max(0,i-1)*lyrHeight)*sin(angle/180*pi),height];
