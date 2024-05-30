759ebd4850493a829abf508d7b591455aab93d5a
CHEN-Yuanzhi <84832730+CHEN-Yuanzhi@users.noreply.github.com>
2024-05-30 08:47:21 +0800

    0530

---
+++ CHENYuanzhi/monitoring/double_spiral.m
@@ -0,0 +1,65 @@
+%%%%%%%%%%%%%%%%%%%
+%
+% date: 2023-5-15
+% author: CHEN Yuanzhi
+%
+%%%%%%%%%%%%%%%%%%
+
+% file param:
+P_pattern = ["const", "tooth", "sin", "square"];
+F_pattern = ["const", "tooth", "sin", "square"];
+
+% printing process param
+pwr = 200; % 1.2KW / 4kw *1000;
+pFeedrate = 400; % mm/min
+lenPos = 600;
+flowL = 250; % 6 L/min / 20L/min * 1000;
+speedL = 100;% 2 r/min / 10r/min * 1000;
+flowR = 400;% 6 L/min / 20L/min * 1000;
+speedR = 100;% 2 r/min / 10r/min * 1000;
+safetyHeight = 25;
+
+%  geometry param
+traverse=2000;
+% shape
+handle = doublespiralsinglelayer;
+
+%%
+%%%%%%%%%%%%%% printing path
+%%%% the regular code for generate a script
+for i=1:4
+    for j=1:4
+        num1 = randi([0, 1], 1);
+        num2 = mod(num1 + 1, 2); 
+        Rtcp_use = [num1,num2];
+        hFilename = strcat('./DoubleSpiral_',P_pattern(i),'_',F_pattern(j),'.txt');
+        pg = cPathGen(hFilename); % create the path generator object
+        pg.genNewScript();
+        %%% Beam1
+        pg.changeMode(1); % change to printing mode
+        [pPathSeq,pwrSeq, pFeedrateSeq,vertices] = handle.genPrintingPath(pwr, pFeedrate, traverse, P_pattern(i), F_pattern(j), Rtcp_use);
+        lenPosSeq = ones(length(pPathSeq),1) * lenPos;
+        %%%%%%%%%%%%% following for path Gen %%%%%%%%%%%%%%%%%%%%%
+        %%% start printing mode
+        pg.setLaser(0, lenPos, flowL, speedL, flowR, speedR); % set a init process param (in case of overshoot)
+        pg.startRTCP(safetyHeight, 16); 
+        pg.addPathPts([0,0,safetyHeight,0,45], 3000);
+        pg.addPathPts([0,0,safetyHeight,0,45], 3000);
+        pg.saftyToPt([nan, nan, safetyHeight], pPathSeq(1,:), 500); % safety move the start pt
+        pg.enableLaser(1, 20);
+        %%% add path pts
+        pg.addPathPtsWithPwr(pPathSeq, pwrSeq, lenPosSeq, pFeedrateSeq);
+        %%% exist printing mode
+        pg.disableLaser(1);
+        pg.stopRTCP(safetyHeight, 16); 
+        pg.addPathPts([-110,-5,120,0,45], 1000);
+        %%% draw
+        pg.draw_ = false;
+        pg.drawPath(pPathSeq, pPathSeq);
+        saveName=strcat('DoubleSpiral_',P_pattern(i),'_',F_pattern(j),'.mat');
+        save(saveName,'pPathSeq','pwrSeq','pFeedrateSeq');
+        %% end the script
+        pg.closeScript();
+    end
+end
+save('DoubleSpiral_vertices.mat','vertices');
---
+++ lib/shape/doublespiralsinglelayer.m
@@ -0,0 +1,121 @@
+% date: 20230515
+% functions for incline single channel wall
+% author: Yuanzhi CHEN
+
+classdef doublespiralsinglelayer
+	properties
+        shape_="Wall";
+    end
+    
+    methods(Static)
+        function [path, pwrSeq,feedSeq,vertices] = genPrintingPath(pwr, fr, traverse, pPattern, fPattern, Rtcp)
+            path = [];
+            pwrSeq = [];
+            feedSeq = [];
+            vertices = [];
+            zoffset=5.8147;
+            lead = 0.01;
+            
+            %spiral generation
+            Length=900;
+            sample=0.1;
+            n=[0:sample:Length];
+            w=2*pi;
+            b=4;            
+            a1=3;
+            t1=(sqrt((w*a1)^2+2*b*w^2.*n)-a1*w)/(b*w^2);
+            zeta1=w*t1;
+            r1=a1+b*zeta1;
+            x1=r1.*cos(zeta1);
+            y1=r1.*sin(zeta1);
+            a2=15.5;
+            t2=(sqrt((w*a2)^2+2*b*w^2.*n)-a2*w)/(b*w^2);
+            zeta2=w*t2;
+            r2=a2+b*zeta2;
+            x2=r2.*cos(zeta2);
+            y2=r2.*sin(zeta2);
+            x2=fliplr(x2);
+            y2=fliplr(y2);            
+%             plot(x1, y1); % 绘制螺旋线
+%             axis equal; % 保持坐标轴比例一致
+%             hold on
+%             plot(x2,y2);
+
+            %power & feedrate generation
+            num=Length/sample+1;
+            power=doublespiralsinglelayer.getPower(pPattern,num)*pwr;
+            feedrate=doublespiralsinglelayer.getFeedrate(fPattern,num)*fr;
+            if strcmp(fPattern,"square")
+                tempFeedrate=feedrate/60;
+                tempFeedrate=timeSmooth(tempFeedrate');
+                feedrate=(tempFeedrate*60)';
+            end
+            zeta1=zeta1';
+            zeta2=zeta2';
+            if Rtcp(1)==0
+                zeta1=zeros(num,1);
+            end
+            if Rtcp(2)==0
+                zeta2=zeros(num,1);
+            end
+            path=[path;0,0,zoffset,0,0];
+            pwrSeq=[pwrSeq,0];
+            feedSeq=[feedSeq,traverse];
+            path=[path;x1(1)-lead,y1(1)-lead,zoffset,0,zeta1(1)/(2*pi)*360];
+            pwrSeq=[pwrSeq,0];
+            feedSeq=[feedSeq,feedrate(1)];
+            path=[path;x1',y1',ones(num,1)*zoffset,zeros(num,1),ones(num,1).*zeta1/(2*pi)*360];
+            vertices = [vertices;x1',y1',ones(num,1)*zoffset,zeros(num,1),ones(num,1).*zeta1/(2*pi)*360];
+            pwrSeq=[pwrSeq,power];
+            feedSeq=[feedSeq,feedrate];
+            path=[path;x1(end)-lead,y1(end),zoffset,0,zeta1(end)/(2*pi)*360];
+            pwrSeq=[pwrSeq,0];
+            feedSeq=[feedSeq,feedrate(end)];
+            path=[path;x2(1)+lead,y2(1)+lead,zoffset,0,zeta2(1)/(2*pi)*360];
+            pwrSeq=[pwrSeq,0];
+            feedSeq=[feedSeq,feedrate(end)];
+            path=[path;x2',y2',ones(num,1)*zoffset,zeros(num,1),ones(num,1).*zeta2/(2*pi)*360];
+            vertices = [vertices;x2',y2',ones(num,1)*zoffset,zeros(num,1),ones(num,1).*zeta2/(2*pi)*360];
+            pwrSeq=[pwrSeq,power];
+            feedSeq=[feedSeq,feedrate]; 
+            pwrSeq=pwrSeq';
+            feedSeq=feedSeq';
+            EDR=(pwrSeq*4)./(feedSeq/60);
+            max(EDR)
+            min(EDR(EDR~=0))
+        end
+        
+        function power = getPower(pPattern, num)
+            power=zeros(4,num);           
+            t=1:num;
+            t=t/num.*(2*pi*(1+0.001*t));
+            if strcmp(pPattern, "const")
+                power=zeros(1,num);  
+            elseif strcmp(pPattern, "tooth")
+                power=sawtooth(t,0.5);
+            elseif strcmp(pPattern, "sin")
+                power=sin(t);
+            elseif strcmp(pPattern, "square")
+                power=square(t);
+                power(end)=power(end-1);
+            end
+            power=power*0.3/2+1;
+        end
+        
+        function feedrate = getFeedrate(fPattern,num)        
+            t=1:num;
+            t=t/num.*(2*pi*(1+0.001*t))-0.5*pi;
+            if strcmp(fPattern, "const")
+                feedrate=zeros(1,num);  
+            elseif strcmp(fPattern, "tooth")
+                feedrate=sawtooth(t,0.5);
+            elseif strcmp(fPattern, "sin")
+                feedrate=sin(t);
+            elseif strcmp(fPattern, "square")
+                feedrate=square(t);
+                feedrate(end)=feedrate(end-1);
+            end
+            feedrate=feedrate*0.3/2+1;
+        end
+    end
+end
