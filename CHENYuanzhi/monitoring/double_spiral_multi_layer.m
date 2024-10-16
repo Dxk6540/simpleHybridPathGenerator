d025d3be573e1b20c6f804c33dd47a7f4346df3d
EricZHANG0123 <zhangwenze0123@outlook.com>
2024-10-16 19:31:04 +0800

    E

--- CHENYuanzhi/monitoring/double_spiral_multi_layer.m
+++ CHENYuanzhi/monitoring/double_spiral_multi_layer.m
@@ -6,14 +6,14 @@
 %%%%%%%%%%%%%%%%%%
 
 % file param:
-P_pattern = ["const", "tooth", "sin", "square","noise"];
-F_pattern = ["const", "tooth", "sin", "square","noise"];
+P_pattern = ["const", "tooth", "sin","noise"];
+F_pattern = ["const", "tooth", "sin","noise"];
 % printing process param
 pwr = 200; % 1.2KW / 4kw *1000;
 pFeedrate = 600; % mm/min
 lenPos = 800;
 flowL = 250; % 6 L/min / 20L/min * 1000;
-speedL = 60;% 2 r/min / 10r/min * 1000;
+speedL = 100;% 2 r/min / 10r/min * 1000;
 flowR = 400;% 6 L/min / 20L/min * 1000;
 speedR = 100;% 2 r/min / 10r/min * 1000;
 safetyHeight = 35;
@@ -22,12 +22,14 @@ traverse=2000;
 B_axis=randB(2*length(P_pattern)*length(F_pattern));
 % shape
 handle = doublespiralMultiplelayer;
-Z_coord = -0.031*ones(1,8002);
+load('Z');
+Z_coord=data'*3;
+% Z_coord = -0.98*ones(1,8002);
 %%
 %%%%%%%%%%%%%% printing path
 %%%% the regular code for generate a script
-i=5;
-j=5;
+i=4;
+j=4;
 Rtcp_use = [1,1];
 hFilename = strcat('./DoubleSpiralMultiLayer_',P_pattern(i),'_',F_pattern(j),'.txt');
 pg = cPathGen(hFilename); % create the path generator object
@@ -35,7 +37,7 @@ pg.genNewScript();
 %%% Beam1
 pg.changeMode(1); % change to printing mode
 
-[pPathSeq,pwrSeq, pFeedrateSeq,vertices] = handle.genPrintingPath(pwr, pFeedrate, traverse, P_pattern(i), F_pattern(j), Rtcp_use, B_axis(5*i+j-5,:), Z_coord);
+[pPathSeq,pwrSeq, pFeedrateSeq,vertices] = handle.genPrintingPath(pwr, pFeedrate, traverse, P_pattern(i), F_pattern(j), Rtcp_use, B_axis(4*i+j-4,:), Z_coord);
 lenPosSeq = ones(length(pPathSeq),1) * lenPos;
 %%%%%%%%%%%%% following for path Gen %%%%%%%%%%%%%%%%%%%%%
 %%% start printing mode
@@ -67,7 +69,7 @@ rng(123); % 替换为你希望使用的种子
 
 % 定义可选数字
 options = [15, 15, 15, 15, 15];
-
+% options = [0, 0, 0, 0, 0];
 % 生成 20 个随机整数
 randNumbers = options(randi(length(options), 1, numbers));
 
